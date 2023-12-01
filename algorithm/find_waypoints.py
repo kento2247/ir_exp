480612ba1382424c43b0b6178832dae8032e288b
KentoTokura <tkento1985@icloud.com>
2023-12-01 16:16:40 +0900

    update find function

--- algorithm/find_waypoints.py
+++ algorithm/find_waypoints.py
@@ -12,7 +12,7 @@ class PathPlanning:
         self.mesh_end = {"x": 3.2, "y": 3.5}
         self.mesh_resolution = 0.1
         self.obstacle_coordinates = obstacle_coordinates
-        self.colision_width = 0.2
+        self.colision_width = 0.15
         self.begin_point = {"x": 2.5, "y": 1.85, "theta": 90}
         self.end_point = {"x": 2.0, "y": 3.5, "theta": 90}
         self.begin_index = {"x": 0, "y": 0}
@@ -192,7 +192,7 @@ def get_waypoints(obstacle_coordinates):
     print(obstacle_coordinates)
     if obstacle_coordinates is None:
         return None
-    obstacle_list = [{"x": 2.0, "y": 2.1, "z": 0.0}]
+    obstacle_list = [{"x": 2.0, "y": 2.1, "z": 0.0}, {"x": 2.3, "y": 2.1, "z": 0.0}]
     for i in obstacle_coordinates:
         print(i)
         obstacle_list.append({"x": i.x, "y": i.y, "z": 0.0})
@@ -213,14 +213,13 @@ def get_waypoints(obstacle_coordinates):
     return return_list
 
 
-"""
 def test():
     obstacle_coordinates = [
         {"x": 2.56485074506, "y": 2.43516755659, "z": 0.040733772599},
         {"x": 2.98526708458, "y": 2.77542427319, "z": 0.0266928948159},
         {"x": 2.98471341612, "y": 2.77432731505, "z": 0.0278568250358},
     ]
-    obstacle_list = [{"x": 2.0, "y": 2.1, "z": 0.0}]
+    obstacle_list = [{"x": 2.3, "y": 2.1, "z": 0.0}, {"x": 2.0, "y": 2.1, "z": 0.0}]
     for i in obstacle_coordinates:
         obstacle_list.append(i)
     path_planning = PathPlanning(obstacle_list)
@@ -248,5 +247,5 @@ def test():
     print(return_list)
     return return_list
 
-test() 
-"""
+
+test()
