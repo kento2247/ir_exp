205d95a52d903ebc15a214668d6358dd0d3f9f48
KentoTokura <tkento1985@icloud.com>
2023-12-01 15:49:32 +0900

    bug fix

--- algorithm/find_waypoints.py
+++ algorithm/find_waypoints.py
@@ -11,9 +11,7 @@ class PathPlanning:
         self.mesh_begin = {"x": 2.0, "y": 1.8}
         self.mesh_end = {"x": 3.2, "y": 3.5}
         self.mesh_resolution = 0.1
-        for i in obstacle_coordinates:
-            self.obstacle_coordinates.append({"x": i.x, "y": i.y, "z": 0.0})
-        self.obstacle_coordinates.append({"x": 2.1, "y": 2.1, "z": 0.0})
+        self.obstacle_coordinates = obstacle_coordinates
         self.colision_width = 0.15
         self.begin_point = {"x": 2.5, "y": 1.85, "theta": 90}
         self.end_point = {"x": 2.0, "y": 3.5, "theta": 90}
@@ -199,7 +197,11 @@ def get_waypoints(obstacle_coordinates):
     #     {"x": 2.98526708458, "y": 2.77542427319, "z": 0.0266928948159},
     #     {"x": 2.98471341612, "y": 2.77432731505, "z": 0.0278568250358},
     # ]
-    path_planning = PathPlanning(obstacle_coordinates)
+    obstacle_list = [{"x": 2.1, "y": 2.1, "z": 0.0}]
+    for i in obstacle_coordinates:
+        print(i)
+        obstacle_list.append({"x": i.x, "y": i.y, "z": 0.0})
+    path_planning = PathPlanning(obstacle_list)
 
     filtered_waypoints = path_planning.get_waypoints()
     begin = (path_planning.begin_index["x"], path_planning.begin_index["y"])
