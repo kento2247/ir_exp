7eb7e2205940c5b21143368c92ebdf77e76104f7
KentoTokura <tkento1985@icloud.com>
2023-12-04 11:08:35 +0900

    add print

--- algorithm/wrs_main_node.py
+++ algorithm/wrs_main_node.py
@@ -551,7 +551,6 @@ class WrsMainController(object):
         detected_objs = self.get_latest_detection()
         bboxes = detected_objs.bboxes  # [{x:n,y:n,w:n,h:n,label:n,score:n}]
         pos_bboxes = [self.get_grasp_coordinate(bbox) for bbox in bboxes]
-        print("pos_bboxes: ", pos_bboxes)
         path_planning_instance = PathPlanning(pos_bboxes)
         waypoints = path_planning_instance.get_waypoints()
         print("waypoints: ", waypoints)
