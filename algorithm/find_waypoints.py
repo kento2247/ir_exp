291d657c7f7170faaa9a793d0c017f2979940a56
KentoTokura <tkento1985@icloud.com>
2023-12-04 15:45:13 +0900

    fix

--- algorithm/find_waypoints.py
+++ algorithm/find_waypoints.py
@@ -17,7 +17,7 @@ class PathPlanning:
         # self.obstacle_coordinates.append({"x": 2.3, "y": 2.1, "z": 0.0})
         self.colision_width = 0.22
         self.begin_point = {"x": 2.5, "y": 1.85, "theta": 90}
-        self.end_point = {"x": 2.0, "y": 3.5, "theta": 90}
+        self.end_point = {"x": 2.1, "y": 3.5, "theta": 90}
         self.begin_index = {"x": 0, "y": 0}
         self.end_index = {"x": 0, "y": 0}
 
