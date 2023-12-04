b961469b4c21d8cd87b93d4d5d42b0ba28e2ca9e
KentoTokura <tkento1985@icloud.com>
2023-12-04 12:08:52 +0900

    fix

--- algorithm/find_waypoints.py
+++ algorithm/find_waypoints.py
@@ -80,9 +80,7 @@ class PathPlanning:
         begin = (self.begin_index["x"], self.begin_index["y"])
         end = (self.end_index["x"], self.end_index["y"])
         came_from, cost_so_far = self.a_star_search(waypoints, begin, end)
-        filtered_waypoints = self.reconstruct_path(
-            came_from, begin, end, filtered_waypoints
-        )
+        filtered_waypoints = self.reconstruct_path(came_from, begin, end, waypoints)
         # if filtered_waypoints is not None:
         #     for i in range(self.x_time):
         #         for j in range(self.y_time):
